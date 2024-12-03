fbf91139ff85da93710a355562d691835d087c7e
Dj <43033058+DjDeveloperr@users.noreply.github.com>
2024-12-03 15:00:06 -0500

    feat: add App.run

---
+++ runtime/src/App.mm
@@ -0,0 +1,47 @@
+#ifdef __APPLE__
+
+#include "App.h"
+
+#import <AppKit/AppKit.h>
+
+App *App::init(napi_env env) {
+  App *appInst = new App();
+
+  napi_value global, App, app;
+
+  napi_get_global(env, &global);
+
+  napi_create_object(env, &App);
+  napi_set_named_property(env, global, "App", App);
+
+  napi_create_function(env, "run", NAPI_AUTO_LENGTH, run, appInst, &app);
+  napi_set_named_property(env, App, "run", app);
+
+  return appInst;
+}
+
+napi_value App::run(napi_env env, napi_callback_info cbinfo) {
+  App *appInst = nullptr;
+  napi_get_cb_info(env, cbinfo, nullptr, nullptr, nullptr, (void **)&appInst);
+
+  NSApplication *app = [NSApplication sharedApplication];
+
+  [app finishLaunching];
+
+  NSEvent *event;
+
+  while (true) {
+    event = [app nextEventMatchingMask:NSEventMaskAny
+                             untilDate:[NSDate distantFuture]
+                                inMode:NSDefaultRunLoopMode
+                               dequeue:YES];
+    if (event) {
+      [app sendEvent:event];
+      appInst->runtime->drainMicrotasks();
+    }
+  }
+
+  return nullptr;
+}
+
+#endif // __APPLE__
--- runtime/src/Bundle.mm
+++ runtime/src/Bundle.mm
@@ -5,21 +5,22 @@
 #include <Foundation/Foundation.h>
 
 std::string getBundlePath() {
-    NSBundle *mainBundle = [NSBundle mainBundle];
-    NSString *bundlePath = [mainBundle bundlePath];
-    return [bundlePath UTF8String];
+  NSBundle *mainBundle = [NSBundle mainBundle];
+  NSString *bundlePath = [mainBundle bundlePath];
+  return [bundlePath UTF8String];
 }
 
 std::string getBytecodePathFromBundle() {
-    NSBundle *mainBundle = [NSBundle mainBundle];
-    if ([mainBundle objectForInfoDictionaryKey:@"NativeScriptApplication"] == nil) {
-        return "";
-    }
-    NSString *bytecodePath = [mainBundle pathForResource:@"app" ofType:@"hbc"];
-    if (bytecodePath == nil) {
-        return "";
-    }
-    return [bytecodePath UTF8String];
+  NSBundle *mainBundle = [NSBundle mainBundle];
+  if ([mainBundle objectForInfoDictionaryKey:@"NativeScriptApplication"] ==
+      nil) {
+    return "";
+  }
+  NSString *bytecodePath = [mainBundle pathForResource:@"app" ofType:@"hbc"];
+  if (bytecodePath == nil) {
+    return "";
+  }
+  return [bytecodePath UTF8String];
 }
 
 #endif // __APPLE__
